ac0a0a57dd3cb74ee6f6fa512c0df630b4a20e98
LoxCreamBagel <loxonbagels@gmail.com>
2023-01-29 23:01:39 -0800

    Set up dynamics routes

---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+// The dynamics path-page file must contain
+// 1. A React component to render the page
+// 2. getStaticPaths which returns an array of possible values
+// 3. getStaticProps which fetches necessary data
+
+const Post = ({ postData }) => {
+    return (
+    <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+    </Layout>
+    );
+};
+
+// Called 1st
+export const getStaticPaths = () => {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    };
+};
+
+// Called 2nd with result from getStaticPaths
+export const getStaticProps = ({ params }) => {
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData
+        }
+    };
+};
+
+export default Post;
